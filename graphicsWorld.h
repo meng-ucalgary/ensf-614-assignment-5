97931a362ac7c591ffcd1b406eb47d86aca05ad2
Bhavyai Gupta <zbhavyai@gmail.com>
2021-10-24 02:09:10 -0600

    implementation of graphicsWorld

---
+++ graphicsWorld.h
@@ -0,0 +1,19 @@
+/*
+ * File Name:               graphicsWorld.h
+ * Course:                  ENSF 614 - Fall 2021
+ * Lab # and Assignment #:  Lab 5 Exercise B
+ * Lab section:             B01
+ * Completed by:            Aastha Patel, Bhavyai Gupta
+ * Submission Date:         October 26, 2021
+ */
+
+#ifndef GRAPHICSWORLD_H
+#define GRAPHICSWORLD_H
+
+class GraphicsWorld
+{
+public:
+    static void run();
+};
+
+#endif

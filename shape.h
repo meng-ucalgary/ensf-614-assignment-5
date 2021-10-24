71811fb2fb0376d69691f281dbe3039494001e7a
Bhavyai Gupta <zbhavyai@gmail.com>
2021-10-24 15:25:53 -0600

    added documentation comments

--- shape.h
+++ shape.h
@@ -20,28 +20,63 @@ protected:
 
 public:
     Shape(double x, double y, const char *shapeName);
+    // REQUIRES
+    //    two arguments of type double and a pointer to built-in string
+    // PROMISES
+    //    creates Shape object with the supplied arguments
 
     virtual ~Shape();
+    // PROMISES
+    //    destroys the Shape object
+    //    deallocates the memory referenced by shapeName
 
     Shape(const Shape &s);
+    // REQUIRES
+    //    reference of Shape object as argument s
+    // PROMISES
+    //    creates Shape object with deep copy of data members of s
 
     Shape& operator=(const Shape &rhs);
+    // REQUIRES
+    //    reference of Shape object on right hand side of =
+    // PROMISES
+    //    deep copy of data members of rhs to object being created
 
     const Point &getOrigin() const;
+    // PROMISES
+    //    returns reference to origin
 
     const char *getName() const;
+    // PROMISES
+    //    returns pointer to shapeName
 
     virtual void display() const;
+    // PROMISES
+    //    prints the Shape object on stdout
 
-    virtual double distance(Shape &other) const;
+    virtual double distance(Shape &S) const;
+    // REQUIRES
+    //    reference to Shape object
+    // PROMISES
+    //    returns the distance between this Shape and S on the cartesian plane
 
-    static double distance(Shape &the_shape, Shape &other);
+    static double distance(Shape &S, Shape &T);
+    // REQUIRES
+    //    two references to Shape objects as arguments
+    // PROMISES
+    //    returns the distance between S and T on the cartesian plane
 
     virtual double area() const = 0;
+    // PROMISES
+    //    returns the area of the Shape
 
     virtual double perimeter() const = 0;
+    // PROMISES
+    //    returns the perimeter of the Shape
 
     void move (double dx, double dy);
+    // PROMISES
+    //    updates the origin of Shape by moving co-ordinates by (dx, dy)
 };
 
 #endif

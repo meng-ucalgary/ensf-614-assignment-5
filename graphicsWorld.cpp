86944ba8a997fd5928e8d6b70ce5cbebdf62893d
Bhavyai Gupta <zbhavyai@gmail.com>
2021-10-25 23:06:26 -0600

    added code for exercise C

--- graphicsWorld.cpp
+++ graphicsWorld.cpp
@@ -1,13 +1,15 @@
 /*
  * File Name:               graphicsWorld.cpp
  * Course:                  ENSF 614 - Fall 2021
- * Lab # and Assignment #:  Lab 5 Exercise B
+ * Lab # and Assignment #:  Lab 5 Exercise B and C
  * Lab section:             B01
  * Completed by:            Aastha Patel, Bhavyai Gupta
  * Submission Date:         October 26, 2021
  */
 
 #include "graphicsWorld.h"
+#include "curvecut.h"
+#include "circle.h"
 #include "rectangle.h"
 #include "square.h"
 #include "shape.h"
@@ -20,6 +22,10 @@ void GraphicsWorld::run()
 {
     cout << "Authors: Aastha Patel, Bhavyai Gupta" << endl;
 
+    cout << "\n\n+----------------+\n";
+    cout << "|   EXERCISE B   |\n";
+    cout << "+----------------+\n\n";
+
     // #if 0  // Change 0 to 1 to test Point
     Point m(6, 8);
     Point n(6, 8);
@@ -29,6 +35,7 @@ void GraphicsWorld::run()
     cout << "\nExpected second version of the distance function also print: 3";
     cout << "\nThe distance between m and n is again: " << Point::distance(m, n);
     // #endif // end of block to test Point
+
 
     // #if 0  // Change 0 to 1 to test Square
     cout << "\n\nTesting Functions in class Square:" << endl;
@@ -96,4 +103,54 @@ void GraphicsWorld::run()
     sh[2]->display();
     sh[3]->display();
     // #endif // end of block to test array of pointer and polymorphism
+
+
+    cout << "\n\n+----------------+\n";
+    cout << "|   EXERCISE C   |\n";
+    cout << "+----------------+\n\n";
+
+
+
+    // #if 0
+    cout << "\nTesting Functions in class Circle:" << endl;
+    Circle c(3, 5, 9, "CIRCLE C");
+    c.display();
+    cout << "the area of " << c.getName() << " is: " << c.area() << endl;
+    cout << "the perimeter of " << c.getName() << " is: " << c.perimeter() << endl;
+    d = a.distance(c);
+    cout << "\nThe distance between rectangle a and circle c is: " << d << endl;
+
+    CurveCut rc(6, 5, 10, 12, 9, "CurveCut rc");
+    rc.display();
+    cout << "the area of " << rc.getName() << " is: " << rc.area() << endl;
+    cout << "the perimeter of " << rc.getName() << " is: " << rc.perimeter();
+    d = rc.distance(c);
+    cout << "\nThe distance between rc and c is: " << d << endl;
+    // Using array of Shape pointers:
+    // Shape *sh[4];
+    sh[0] = &s;
+    sh[1] = &a;
+    sh[2] = &c;
+    sh[3] = &rc;
+    sh[0]->display();
+    cout << "The area of " << sh[0]->getName() << " is: " << sh[0]->area();
+    cout << "\nthe perimeter of " << sh[0]->getName() << " is: " << sh[0]->perimeter() << endl << endl;
+    sh[1]->display();
+    cout << "\nThe area of " << sh[1]->getName() << " is: " << sh[1]->area();
+    cout << "\nthe perimeter of " << sh[0]->getName() << " is: " << sh[1]->perimeter() << endl << endl;
+    sh[2]->display();
+    cout << "\nThe area of " << sh[2]->getName() << " is: " << sh[2]->area();
+    cout << "\nthe circumference of " << sh[2]->getName() << " is: " << sh[2]->perimeter() << endl << endl;
+    sh[3]->display();
+    cout << "\nThe area of " << sh[3]->getName() << " is: " << sh[3]->area();
+    cout << "\nthe perimeter of " << sh[3]->getName() << " is: " << sh[3]->perimeter() << endl << endl;
+    cout << "\nTesting copy constructor in class CurveCut:" << endl;
+    CurveCut cc = rc;
+    cc.display();
+    cout << "\nTesting assignment operator in class CurveCut:" << endl;
+    CurveCut cc2(2, 5, 100, 12, 9, "CurveCut cc2");
+    cc2.display();
+    cc2 = cc;
+    cc2.display();
+    // #endif
 }

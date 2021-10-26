d075920f0fa3033b329efa41741b818c9a1fadff
Bhavyai Gupta <zbhavyai@gmail.com>
2021-10-25 23:04:11 -0600

    comment formatting updates

--- circle.h
+++ circle.h
@@ -2,56 +2,50 @@
  * File Name:               circle.h
  * Course:                  ENSF 614 - Fall 2021
  * Lab # and Assignment #:  Lab 5 Exercise C
- * Lab section:             C01
+ * Lab section:             B01
  * Completed by:            Aastha Patel, Bhavyai Gupta
  * Submission Date:         October 26, 2021
  */
 
-
-#include "shape.h"
 #include "rectangle.h"
+#include "shape.h"
+
 #ifndef CIRCLE_H
 #define CIRCLE_H
 
 class Circle : virtual public Shape
 {
-
 protected:
-
     double radius;
 
 public:
-
     Circle(double x, double y, double r, const char *shapeName);
     // REQUIRES
-    // three args x,y for origin r for radius and a char pointer for name
+    //    three args x,y for origin r for radius and a char pointer for name
     // PROMISES
-    // create circle from the given args
+    //    create Circle object from the given args
 
     double area() const;
-
-    //PROMISES
-    // calculate & return the area of circle
+    // PROMISES
+    //    calculate & return the area of circle
 
     double perimeter() const;
-
-    //PROMISES
-    // calculate & return the perimeter of circle
+    // PROMISES
+    //    calculate and returns the perimeter of circle
 
     double get_radius() const;
-
-    //PROMISES
-    // return the radius of circle
+    // PROMISES
+    //    return the radius of circle
 
     void set_radius(double radius);
     // REQUIRES
-    // double arg for radius
-
-    //PROMISES
-    // set the radius of circle
+    //    double arg for radius
+    // PROMISES
+    //    set the radius of circle
 
     void display() const;
-    //PROMISES
-    // display shape's origin, area,perimeter and name
+    // PROMISES
+    //    display Circle's origin, area, perimeter and name
 };
+
 #endif

6b1edf0155f4e273da46e2631adf9416cc34b424
Bhavyai Gupta <zbhavyai@gmail.com>
2021-10-25 23:06:12 -0600

    fixed logic, assertion, and formatting

--- curvecut.cpp
+++ curvecut.cpp
@@ -2,7 +2,7 @@
  * File Name:               curvecut.cpp
  * Course:                  ENSF 614 - Fall 2021
  * Lab # and Assignment #:  Lab 5 Exercise C
- * Lab section:             C02
+ * Lab section:             B01
  * Completed by:            Aastha Patel, Bhavyai Gupta
  * Submission Date:         October 26, 2021
  */
@@ -11,41 +11,37 @@
 #include "circle.h"
 #include "shape.h"
 #include "point.h"
+
 #include <stdio.h>
 #include <math.h>
-#define PI 3.14
-
 using namespace std;
 
-CurveCut :: CurveCut(double x, double y, double w,double l , double r,const char *shapeName) : Circle(x,y,r,shapeName),Rectangle(x,y,w,l,r,shapeName)
+CurveCut::CurveCut(double x, double y, double w, double l, double r, const char *shapeName) : Shape(x, y, shapeName), Circle(x, y, r, shapeName), Rectangle(x, y, w, l, shapeName)
 {
-    this-> set_width(w);
+    double minLength = w < l ? w : l;
+
+    if (r > minLength)
+    {
+        fprintf(stderr, "\n[FAIL] The radius of the circle must be always less than or equal the smaller of the width and length. Exit!\n");
+        exit(1);
+    }
 }
 
-double CurveCut :: area() const
+double CurveCut::area() const
 {
-    return ((this->get_side_a() * this->get_width())) - ((PI * (sqrt(this->get_radius()))) / 4 )  ;
+    return (Rectangle::area() - (Circle::area() / 4));
 }
 
-double CurveCut :: perimeter() const
+double CurveCut::perimeter() const
 {
-    return (2*(this->get_side_a() + this->get_side_b())) - ((2*PI*this->radius)/4);
+    return Rectangle::perimeter() - (2 * this->get_radius()) + (Circle::perimeter() / 4);
 }
 
-double CurveCut :: get_width() const
-{
-    return this->width;
-}
-
-
-void CurveCut :: display() const
+void CurveCut::display() const
 {
     printf("CurveCut Name: %s\n", this->getName());
     this->getOrigin().display();
-    printf("Radius is : %.2f\n", this->get_radius());
-    printf("width is : %.2f\n", this->get_width());
-    printf("Area: %.2f\n", this->area());
-    printf("Perimeter: %.2f\n", this->perimeter());
+    printf("Width: %.2f\n", this->get_side_a());
+    printf("Length: %.2f\n", this->get_side_b());
+    printf("Radius of the cut: %.2f\n", this->get_radius());
 }
-
-

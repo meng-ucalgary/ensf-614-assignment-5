6f6129fe9a6cb1239afcbf92f383620ac9195bd6
Bhavyai Gupta <zbhavyai@gmail.com>
2021-10-25 23:06:48 -0600

    adding driver main for exercise C

---
+++ lab5ExC.cpp
@@ -0,0 +1,18 @@
+/*
+ * File Name:               lab5ExC.cpp
+ * Course:                  ENSF 614 - Fall 2021
+ * Lab # and Assignment #:  Lab 5 Exercise C
+ * Lab section:             B01
+ * Completed by:            Aastha Patel, Bhavyai Gupta
+ * Submission Date:         October 26, 2021
+ */
+
+#include "graphicsWorld.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    GraphicsWorld::run();
+    return 0;
+}
